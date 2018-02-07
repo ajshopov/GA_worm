c9b3f9e18a9e0d4df46c5b465e35d9ff80ce889c
ajshopov <alexanderjshopov@gmail.com>
2018-02-08 00:06:29 +1100

    chart now plotting moving averages

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -7,9 +7,10 @@ var options = {
 };
 
 var allVotes;
-var chartData = [];
+var last30sVotesRuby = [];
 var timeline = [];
 var myChart;
+var data;
 
 var option = {
   responsive: false,
@@ -37,23 +38,12 @@ var option = {
 // inside event listener run ajax to update ruby
 // use myChart.update() to include pushed data
 
-  $.ajax(options).done(function(allVotes){
-    allVotes. forEach(function(vote){
-      chartData.push(vote['vote_value']);
-      console.log(chartData);
 
-      var chartSum = chartData.reduce(function getSum(total, num){
-        return total + num;
-      })
 
-      var average = chartSum/chartData.length
-      console.log(average)
+$(document).ready(function() {
+    var ctx = document.getElementById('chart_test');
 
-      timeline.push(average);
-      console.log("timeline " + timeline)
-    });
-
-    var data = {
+    data = {
       labels: [1,2,3,4,5,6,7,8,9,10,11,12],
       datasets: [{
         label: "Dataset #1",
@@ -65,28 +55,49 @@ var option = {
         data: timeline,
       }]
     };
-
-    $(document).ready(function() {
-      var ctx = document.getElementById('chart_test');
-
-      myChart = new Chart(ctx, {
-        type: 'line',
-        options: option,
-        data: data
-      })
+    myChart = new Chart(ctx, {
+      type: 'line',
+      options: option,
+      data: data
     })
+
+})
+
+
+
+
+setInterval(function(){
+
+  $.ajax(options).done(function(allVotes){
+    last30sVotesRuby = [];
+    allVotes. forEach(function(vote){
+      last30sVotesRuby.push(vote['vote_value']);
+      console.log('last30sVotesRuby' + last30sVotesRuby);
+    });
+      // wrap reduce in if statement for no new votes
+      var sum = last30sVotesRuby.reduce(function getSum(total, num){
+        return total + num;
+      })
+
+      var meanVote = sum/last30sVotesRuby.length
+      console.log("meanVote" + meanVote)
+
+      if (timeline.length >= 12) {
+        timeline.shift();
+      }
+      timeline.push(meanVote);
+      console.log("timeline " + timeline)
+  myChart.update();
   });
+
+}, 10000);
+
+    // event.preventDefault();
+  // })
+  // })
 
 
 
 
 // [12,20,30,80,90,70,50,60,30,30,40,10]
-
-
-
-// Chart.Line('chart_test', {
-//   options: option,
-//   data: data
-// });
-
 
